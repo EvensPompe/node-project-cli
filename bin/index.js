a5411001ca529044dab7b1e0abfd4faa21446a68
Evens Pompe <evenspompe9593@gmail.com>
2022-06-06 21:32:30 +0200

    feat(index): add create project command ,try catch, execPromise function and fs/promise

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,73 @@
 #!/usr/bin/env node
+import { mkdir, writeFile, readFile } from "fs/promises";
 import { createRequire } from "module";
+import { exec } from "child_process";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const require = createRequire(import.meta.url);
 import chalk from "chalk";
 const pkg = require("../package");
 const [, , ...args] = process.argv;
-if (args.length > 0) {
-  if (args.length === 1) {
-    const [arg] = args;
-    switch (arg) {
-      case "--version":
-      case "-v":
-        const versionColored = chalk.white(pkg.version);
-        console.log(versionColored);
-        break;
-      default:
-        const errorMessage = chalk.red("Unknown command");
-        console.log(errorMessage);
-        break;
+const execPromise = (path) =>
+  new Promise((resolve, reject) => {
+    exec(path, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stdout || stderr);
+      }
+    });
+  });
+try {
+  if (args.length > 0) {
+    if (args.length === 1) {
+      const [arg] = args;
+      switch (arg) {
+        case "--version":
+        case "-v":
+          const versionColored = chalk.white(pkg.version);
+          console.log(versionColored);
+          break;
+        default:
+          const errorMessage = chalk.bgRed.black("Unknown command");
+          console.log(errorMessage);
+          break;
+      }
+    } else {
+      if (args.length === 2) {
+        const [arg1, arg2] = args;
+        switch (arg1) {
+          case "create":
+            const startCreate = chalk.bgBlue.black(
+              `Create new node project: ${arg2}`
+            );
+            const endCreate = chalk.bgGreen.black(`Project ${arg2} finished`);
+            console.log(startCreate);
+            await mkdir(`${process.cwd()}/${arg2}`);
+            await execPromise(`cd ${arg2} && npm init -y`);
+            await execPromise(`cd ${arg2} && git init`);
+            const dataAppReaded = await readFile(
+              join(__dirname, "./create/dataApp.js")
+            );
+            await writeFile(
+              `${process.cwd()}/${arg2}/app.js`,
+              dataAppReaded.toString()
+            );
+            console.log(endCreate);
+            break;
+          default:
+            const errorMessage = chalk.bgRed.black("Unknown command");
+            console.log(errorMessage);
+            break;
+        }
+      }
     }
-    process.exit(0);
   }
+  process.exit(0);
+} catch (error) {
+  const errorProcess = chalk.bgRed.black(error);
+  console.error(errorProcess);
+  process.exit(1);
 }
