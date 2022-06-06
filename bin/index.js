f84bf558660d661395c1a41d9d485fd059089264
Evens Pompe <evenspompe9593@gmail.com>
2022-06-06 16:05:54 +0200

    feat(index): add -v and --version args and error case

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+import chalk from "chalk";
+const pkg = require("../package");
 const [, , ...args] = process.argv;
-console.log("Hello world %o", args);
+if (args.length > 0) {
+  if (args.length === 1) {
+    const [arg] = args;
+    switch (arg) {
+      case "--version":
+      case "-v":
+        const versionColored = chalk.white(pkg.version);
+        console.log(versionColored);
+        break;
+      default:
+        const errorMessage = chalk.red("Unknown command");
+        console.log(errorMessage);
+        break;
+    }
+    process.exit(0);
+  }
+}
