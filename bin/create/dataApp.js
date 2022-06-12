8f7b08bc5afaef27a859d1fde975f8cd59511cdb
Evens Pompe <evenspompe9593@gmail.com>
2022-06-12 13:57:49 +0200

    feat(dataApp): add express package and application

--- bin/create/dataApp.js
+++ bin/create/dataApp.js
@@ -1,1 +1,14 @@
-console.log("hello world :)");
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(8080, () => {
+  console.log("Server turning on : http://localhost:8080");
+});
