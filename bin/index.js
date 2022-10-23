ad7d2cccd5cd78c98bd5bee432eb1d31bc581dc4
Evens Pompe <evenspompe9593@gmail.com>
2022-10-23 13:58:25 +0200

    refactor(bin): refacto condition's cases

--- bin/index.js
+++ bin/index.js
@@ -10,18 +10,29 @@ import Command from "./classes/Command.js";
 const pkg = require("../package");
 const [, , ...args] = process.argv;
 try {
-  if (args.length > 0) {
-    if (args.length === 1) {
-      const [arg] = args;
-      switch (arg) {
-        case "--version":
-        case "-v":
-          const versionColored = chalk.white(pkg.version);
-          console.log(versionColored);
-          break;
-        case "--help":
-        case "-h":
-          const helpLog = chalk.white(`npc <command>
+  if (args.length <= 0) {
+    const helpLog = chalk.white(`npc <command>
+
+    Usage:
+    npc --version -v     get current version
+    npc --help -h        get help about usage and commands
+    npc create <name>    create new node project (by default, a web app with express.js) with name
+    
+    All commands:
+    create,--help,-h,-version,-v`);
+    console.log(helpLog);
+    process.exit(0);
+  }
+  const [arg] = args;
+  switch (arg) {
+    case "--version":
+    case "-v":
+      const versionColored = chalk.white(pkg.version);
+      console.log(versionColored);
+      break;
+    case "--help":
+    case "-h":
+      const helpLog = chalk.white(`npc <command>
 
 Usage:
 npc --version -v     get current version
@@ -30,24 +41,17 @@ npc create <name>    create new node project (by default, a web app with express
 
 All commands:
 create,--help,-h,-version,-v`);
-          console.log(helpLog);
-          break;
-        default:
-          throw new Error("Unknown command");
-      }
-    } else {
+      console.log(helpLog);
+      break;
+    case "create":
       if (args.length === 2) {
-        const [arg1, arg2] = args;
-        switch (arg1) {
-          case "create":
-            const command = new Command();
-            await command.create(arg2);
-            break;
-          default:
-            throw new Error("Unknown command");
-        }
+        const [, arg2] = args;
+        const command = new Command();
+        await command.create(arg2);
       }
-    }
+      break;
+    default:
+      throw new Error("Unknown command");
   }
   process.exit(0);
 } catch (error) {
